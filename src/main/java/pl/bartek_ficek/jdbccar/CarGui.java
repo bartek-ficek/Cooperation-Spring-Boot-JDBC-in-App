b8a33d313e3534af49bfa5e172aadd64e9f822c5
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-06 18:10:18 +0100

    Cargui created. Button dosnn't work so far

---
+++ src/main/java/pl/bartek_ficek/jdbccar/CarGui.java
@@ -0,0 +1,32 @@
+package pl.bartek_ficek.jdbccar;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route
+public class CarGui extends VerticalLayout {
+
+    private TextField textFieldId;
+    private TextField textFieldCarMake;
+    private TextField textFieldModel;
+    private TextField textFieldColor;
+    private Button button;
+    private CarDao carDao;
+
+    @Autowired
+    public CarGui(CarDao carDao) {
+        this.textFieldId = new TextField("Id");
+        this.textFieldCarMake = new TextField("Car Make");
+        this.textFieldModel = new TextField("Model");
+        this.textFieldColor = new TextField("Color");
+        this.button = new Button("Add car");
+        this.carDao = carDao;
+
+        add(textFieldId, textFieldCarMake, textFieldModel, textFieldColor);
+
+
+    }
+}
